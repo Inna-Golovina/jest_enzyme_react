7d940c41d1924a83d6ac831d2a7224f34cbaa1ba
Inna Golovina <igigig978@gmail.com>
2021-07-13 09:22:08 +0300

    fourth

--- src/Info_L7/info.spec.js
+++ src/Info_L7/info.spec.js
@@ -1,4 +1,77 @@
 //тестирования методов жизненного цикла
 
+import { shallow } from "enzyme";
 import React from "react";
 import Info from "./info";
+
+const setUp = () => shallow(<Info />); // у этой функции упрощенная реализация так, как пропсы нам не понадобятся
+
+const componentDidMountSpy = jest.spyOn(Info.prototype, 'componentDidMount'); // на каждый из методов ЖЦ мы вешаем шпиона или м-д spyOn (обертка слежения)
+const componentDidUpdateSpy = jest.spyOn(Info.prototype, 'componentDidUpdate');
+const componentWillUnmountSpy = jest.spyOn(Info.prototype, 'componentWillUnmount');
+
+describe('Info component', () => {
+  let component;
+
+  // мы сначала должны создать наблюдателей, а только затем добавить функции, за котгорыми мы будем следить в компоненте
+
+  beforeEach(() => {
+    jest.spyOn(window, 'addEventListener'); // вешаем такие же отслеживания на методы addEventListener и removeEventListener
+    jest.spyOn(window, 'removeEventListener');
+    component = setUp(); // создаем компонент используя функцию setUp
+  })
+
+  afterEach(() => { // при текущей реализации он необязательный, срабатывае после каждого теста, в нашей логике он сбрасывает замоканые реал-ции в начальное состояние
+    window.addEventListener.mockRestore();
+    window.removeEventListener.mockRestore();
+  })
+
+  it('should render Info component', () => { //
+    expect(component).toMatchSnapshot();
+  })
+
+  describe('Lifecycle methods', () => {
+    it('should call componentDidMount once', () => {
+      expect(componentDidMountSpy).toHaveBeenCalledTimes(1); // мы проверяем чтобы componentDidMount вызывался только 1 раз
+    })
+
+    it('should not call componentWillUnmount when component just mounted', () => {
+      expect(componentDidMountSpy).toHaveBeenCalledTimes(1); // мы проверяем чтобы componentDidMount вызывался только 1 раз
+      expect(componentWillUnmountSpy).toHaveBeenCalledTimes(0); // и не произошла размонтировка
+    })
+
+    it('should call componentDidUpdate', () => {
+      component.setProps(); // мы проверяем действительно ли произошло изменение при получении новых пропсов
+      expect(componentDidUpdateSpy).toHaveBeenCalledTimes(1); // вызвался ли данный метод
+    })
+
+    it('should call componentWillUnmount', () => { // мы проверяем что при розмонтировании компонента вызвался метод componentWillUnmount
+      component.unmount(); // мы проверяем количество вызовов
+      expect(componentWillUnmountSpy).toHaveBeenCalledTimes(1);
+    })
+  })
+
+  describe('Components handlers', () => { // мы тестируем методы компонента, чтобы увеличить покрытие тестами
+    it('should call addEventListener when component mounted', () => { // мы проверяем что этот обработчик вызывается один раз при монтировании
+      expect(window.addEventListener).toHaveBeenCalledTimes(1);
+    })
+
+    it('should call handleChangeTitle in componentDidUpdate', () => { // мы проверяем действительно ли происходит обновление заголовка документа при обновлениии компонента
+      const instance = setUp().instance(); // нам понадобится инстанс, в нем нам нужно замокать метод handleChangeTitle
+      instance.handleChangeTitle = jest.fn(); // делаем это с помощью jest функции
+      instance.componentDidUpdate(); // вызываем метод componentDidUpdate 
+      expect(instance.handleChangeTitle).toHaveBeenCalled(); // проверяем, что handleChangeTitle действительно был вызван
+    })
+
+    it('should call removeEventListener when component unmounted', () => { // мы проверяем розмонтировании компонента обработчик снимается
+      component.unmount();
+      expect(window.removeEventListener).toHaveBeenCalledTimes(1);
+    })
+
+    it('should call call handleWidth during window resize', () => { // мы проверяем, что при изменение размеров окна у нас будет вызываться нужный метод и обновлять значение
+      expect(component.state().width).toBe(0); // до запуска метода мы проверяем действительно ли начальное значение в стейте равно 0
+      global.dispatchEvent(new Event('resize')) // симмулириуем изменение размеров
+      expect(component.state().width).toBe(window.innerWidth) // мы проверяем изменилось ли соответсвенное значение в стейте
+    })
+  })
+})
--- src/Posts_L4/posts.js
+++ src/Posts_L4/posts.js
@@ -4,9 +4,158 @@ import Post from "../Post_Lesson3/post";
 import Title from "../Title_L5/title";
 import Select from "../Select_L5/select";
 import Input from "../Input_L6/input";
+import Pagination from "../Pagination_L9/pagination";
 
-import { NEWS, HITS } from "./constants";
+import "./posts.css";
 
+import {
+  HITS,
+  BASE_PATH,
+  SEARCH_PATH,
+  SEARCH_PARAM,
+  PAGE_HITS,
+  PAGE_PARAM,
+} from "./constants";
+
+class Posts extends Component {
+  state = {
+    searchQuery: "",
+    result: {}, // будет записыватся результат запроса
+    hitsPerPage: 20,
+    page: 0,
+  };
+
+  componentDidMount() { // идет первый запрос на Api
+    const { searchQuery, hitsPerPage, page } = this.state;
+    this.fetchData(searchQuery, hitsPerPage, page);
+  }
+
+  fetchData = (searchQuery, hitsPerPage, page) => { // принимает определенные поля и осуществяет с ними запрос
+    fetch(
+      `${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${searchQuery}&${PAGE_HITS}${hitsPerPage}&${PAGE_PARAM}${page}`
+    )
+      .then((res) => res.json())
+      .then((result) => this.setNews(result))
+  };
+
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({
+      searchQuery: value,
+    });
+  };
+
+  handleHitsChange = ({ target: { value } }) => {
+    const { searchQuery } = this.state;
+
+    this.setState(
+      {
+        hitsPerPage: +value,
+        page: 0,
+      },
+      () => { // тоже добавлен запрос
+        this.fetchData(searchQuery, this.state.hitsPerPage, 0);
+      }
+    );
+  };
+
+  getSearch = ({ key }) => {
+    if (key === "Enter") {
+      const { searchQuery, hitsPerPage } = this.state;
+      this.setState({
+        page: 0,
+      });
+      this.fetchData(searchQuery, hitsPerPage, 0); // тоже добавлен запрос
+    }
+  };
+
+  setNews = (result) => { // устанавлевает полученные данные в state
+    this.setState({ result });
+  };
+
+  handlePageChange = ({ target }) => {
+    const btnType = target.getAttribute("data-name");
+    let { page } = this.state;
+
+    if (!isNaN(btnType)) {
+      this.updatePage(+btnType);
+    } else {
+      if (btnType === "next") {
+        this.updatePage(page + 1);
+      }
+      if (btnType === "prev") {
+        this.updatePage(page - 1);
+      }
+    }
+  };
+
+  updatePage = (page) => {
+    const { searchQuery, hitsPerPage } = this.state;
+    this.setState(
+      {
+        page,
+      },
+      () => {
+        this.fetchData(searchQuery, hitsPerPage, page);
+      }
+    );
+  };
+
+  render() {
+    const { searchQuery, result, hitsPerPage } = this.state;
+    const { hits = [], page, nbPages } = result;
+
+    return (
+      <div className="wrapper">
+        <Title title="Hacker News" />
+        <Select
+          options={HITS}
+          handleChange={this.handleHitsChange}
+          value={hitsPerPage}
+        />
+        {hits.length > 0 && (
+          <Pagination
+            onClick={this.handlePageChange}
+            page={page}
+            lastPage={nbPages}
+          />
+        )}
+        <Input
+          onKeyPress={this.getSearch}
+          onChange={this.handleInputChange}
+          value={searchQuery}
+        />
+        <ul className="newsList">
+          {hits.map(
+            ({
+              author,
+              created_at,
+              num_comments,
+              objectID,
+              title,
+              points,
+              url,
+            }) => (
+              <Post
+                key={objectID}
+                author={author}
+                created_at={created_at}
+                num_comments={num_comments}
+                title={title}
+                points={points}
+                url={url}
+              />
+            )
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Posts;
+
+
+/*
 class Posts extends Component {
   state = {
     searchQuery: "",
@@ -78,5 +227,6 @@ class Posts extends Component {
     );
   }
 }
-
 export default Posts;
+
+*/
