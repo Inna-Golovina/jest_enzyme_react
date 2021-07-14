3ce0f126777b3f408bf25d48fcc752f3c1d386cb
Inna Golovina <igigig978@gmail.com>
2021-07-14 11:25:28 +0300

    sixth

--- src/Posts_L4/posts.spec.js
+++ src/Posts_L4/posts.spec.js
@@ -11,8 +11,10 @@ import {
   PAGE_PARAM,
 } from "./constants";
 
-const mockJsonPromise = Promise.resolve({hits: NEWS, page: 1, nbPages: 10});
-const mockFetchPromise = Promise.resolve({json: () => mockJsonPromise});
+const mockJsonPromise = Promise.resolve({hits: NEWS, page: 1, nbPages: 10}); // мы создаем мок для получаемых данных, мы передаем массив новостей, текущую страницу и общ кол-во страниц
+const mockFetchPromise = Promise.resolve({json: () => mockJsonPromise}); //добавляем поле json, которому присваиваем тестовый ответ
+global.fetch = jest.fn().mockImplementation(() => mockFetchPromise) // с помощью jest function переопределяем глобальный fetch, в дополнение с помощью mockImplementation - описываем имплементацию
+// в данном поле мы можем полностью заменить реализацию метода или функции и контолировать ее работу
 
 const setUp = () => shallow(<Posts />);
 
@@ -29,6 +31,56 @@ describe("Posts component", () => {
   it('should render Posts component', () => {
     expect(component).toMatchSnapshot();
   })
+
+  it('should call fetch in componentDidMount', () => { // проверяем, что припервом монтировании компонента у нас происходит запрос на API
+    expect(global.fetch).toHaveBeenCalledWith(
+      `${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${''}&${PAGE_HITS}${20}&${PAGE_PARAM}${0}`
+      ); // мы проверяем с какими данными у нас происходит вызов метода fetch, мы сконструировали начальную строку запроса (это данные из начального стейта)
+  })
+
+  describe('updatePage method', () => {
+    it('should update "page" value', () => { // проверяем, что при вызове метода и установки ему соответствующего аргумента, этот аргум устанавливается в соответсвующее знач. в стейте
+      instance.updatePage(DEFAULT_PAGE); // для этого мы используем instance, у которого вызываем соответствующий метод
+      expect(component.state().page).toBe(DEFAULT_PAGE); // сравниваем переданное и установленное значение
+    });
+    it('should call fetch with given argument', () => { // мы должны убедиться, что fetch вызывается с переданным аргументом
+      instance.updatePage(DEFAULT_PAGE);
+      expect(global.fetch).toHaveBeenCalledWith( // по аналогии с тестом 'should call fetch in componentDidMount' заменяем значение 0 на константу
+        `${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${''}&${PAGE_HITS}${20}&${PAGE_PARAM}${DEFAULT_PAGE}`
+        );
+    });
+  })
+
+  describe('handlePageChange method', () => { // тестируем метод handlePageChange
+    it('should call "updatePage" with given argument', () => { // метод updatePage вызывает handlePageChange с переданным значением
+      instance.updatePage = jest.fn(); // добавляем mock на updatePage
+      instance.setState({ page: DEFAULT_PAGE })// устанавливаем определенное значение в стейте (по дефолту 10)
+      instance.handlePageChange({
+        target: { getAttribute: jest.fn().mockReturnValue('1') }, // в метод handlePageChange мы передаем замоканые данные события, это объект с полем таргет, внутри которого есть объект с методом getAttribute, данный метод замокан и возвращает нужный нам результат
+      }) // сам мок нужен для коректной отработки строки target.getAttribute("data-name")
+      expect(instance.updatePage).toHaveBeenCalledWith(1)// мы ожидаем, что метод updatePage будет вызван с переданнным ему значением
+    })
+
+    it('should call "updatePage" with increased value', () => { // метод updatePage вызывает handlePageChange с переданным значением prev
+      instance.updatePage = jest.fn(); // добавляем mock на updatePage
+      instance.setState({ page: DEFAULT_PAGE })// устанавливаем определенное значение в стейте (по дефолту 10)
+      instance.handlePageChange({
+        target: { getAttribute: jest.fn().mockReturnValue('prev') }, // в метод handlePageChange мы передаем замоканые данные события, это объект с полем таргет, внутри которого есть объект с методом getAttribute, данный метод замокан и возвращает нужный нам результат
+      }) // сам мок нужен для коректной отработки строки target.getAttribute("data-name")
+      expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE - 1)// мы ожидаем, что метод от значения в стейте будет отниматься 1
+    })
+
+    it('should call "updatePage" with increased value', () => { // метод updatePage вызывает handlePageChange с переданным значением next
+      instance.updatePage = jest.fn(); // добавляем mock на updatePage
+      instance.setState({ page: DEFAULT_PAGE })// устанавливаем определенное значение в стейте (по дефолту 10)
+      instance.handlePageChange({
+        target: { getAttribute: jest.fn().mockReturnValue('next') }, // в метод handlePageChange мы передаем замоканые данные события, это объект с полем таргет, внутри которого есть объект с методом getAttribute, данный метод замокан и возвращает нужный нам результат
+      }) // сам мок нужен для коректной отработки строки target.getAttribute("data-name")
+      expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE + 1)// мы ожидаем, что метод от значения в стейте будет прибавляться 1
+    })
+
+  }) 
+
   describe('Posts handlers', () => { // кейс для тестирования всех хендлеров внутри компонента
     it('should handle search input value', () => {
       expect(component.state().searchQuery).toBe('') // проверяем, чтобы строка в стейте была пустая
